bf0a84249c54e08ebb1299905342a3b2acd1637e
jiyuujin.tsx <jiyuujinunite@gmail.com>
2021-06-22 13:46:26 +0900

    refactor: table style

--- components/TableList.tsx
+++ components/TableList.tsx
@@ -5,24 +5,26 @@ import styles from '../static/Home.module.css'
 export const TableList = ({ items, columns, routingPath }) => {
     return (
         <table className={styles.table}>
-            <tr>
-                {columns.map((column) => (
-                    <th key={column}>{column}</th>
-                ))}
-            </tr>
-            {items.map((item) => {
-                return (
-                    <tr key={item._id}>
-                        {columns.map((column) => (
-                            <td key={column}>
-                                <Link href={`/${routingPath}/${item._id}`}>
-                                    {item[column]}
-                                </Link>
-                            </td>
-                        ))}
-                    </tr>
-                )
-            })}
+            <tbody>
+                <tr>
+                    {columns.map((column) => (
+                        <th key={column}>{column}</th>
+                    ))}
+                </tr>
+                {items.map((item) => {
+                    return (
+                        <tr key={item._id}>
+                            {columns.map((column) => (
+                                <td key={column}>
+                                    <Link href={`/${routingPath}/${item._id}`}>
+                                        {item[column]}
+                                    </Link>
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
         </table>
     )
 }
